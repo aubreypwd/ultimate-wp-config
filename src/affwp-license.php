909e0ac39fe6f8c49e879cc019f336fede447314
Aubrey Portwood <code@aubreypwd.com>
2023-08-15 14:21:24 -0600

    Add ability to use plus license.

--- src/affwp-license.php
+++ src/affwp-license.php
@@ -15,5 +15,6 @@ if (
 	switch ( AFFWP_LICENSE ) {
 		case 'pro':  define( 'AFFILIATEWP_LICENSE_KEY', @defined( 'AFFWP_LICENSE_PRO' ) ? AFFWP_LICENSE_PRO : '' ); break;
 		case 'personal': define( 'AFFILIATEWP_LICENSE_KEY', @defined( 'AFFWP_LICENSE_PERSONAL' ) ? AFFWP_LICENSE_PERSONAL : '' ); break;
+		case 'plus': define( 'AFFILIATEWP_LICENSE_KEY', @defined( 'AFFWP_LICENSE_PLUS' ) ? AFFWP_LICENSE_PLUS : '' ); break;
 	}
 }
